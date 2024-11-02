ba84cde472b11c3013f4670cd0b0b285db1bc7d8
Brandon Rorie <50763955+btror@users.noreply.github.com>
2024-11-01 23:14:33 -0400

    add backspace support

--- wpm.sh
+++ wpm.sh
@@ -2,7 +2,7 @@
 
 # Function to generate a random word
 generate_random_word() {
-  local words=("apple" "banana" "cherry" "date" "elderberry" "fig" "grape" "honeydew" "test" "result" "sweet" "and" "this" "amazing")
+  local words=("apple" "banana" "cherry" "date" "elderberry" "fig" "grape" "honeydew")
   local random_index=$(( $(od -An -N2 -i /dev/urandom) % ${#words[@]} + 1 ))
   echo ${words[$random_index]}
 }
@@ -22,21 +22,21 @@ start_time=$(date +%s)
 end_time=$(( start_time + 5 ))
 random_word=$(generate_random_word)
 user_input=""
-correct_words=0  # Initialize counter
+correct_words=0
 
 display_state
 
 while [ $(date +%s) -lt $end_time ]; do
-  # Calculate remaining time for timeout
   remaining_time=$(( end_time - $(date +%s) ))
-  
-  # Read with timeout - will return 1 if timeout occurs
   read -t $remaining_time -k 1 char || break
 
-  if [[ "$char" == " " ]]; then
-    echo # Print newline after space
+  if [[ "$char" == $'\177' ]]; then  # Check for backspace
+    user_input=${user_input%?}  # Remove last character
+    display_state
+  elif [[ "$char" == " " ]]; then
+    echo
     if [[ "$user_input" == "$random_word" ]]; then
-      correct_words=$((correct_words + 1))  # Increment counter
+      correct_words=$((correct_words + 1))
       random_word=$(generate_random_word)
       user_input=""
       display_state
